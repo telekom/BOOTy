916ca1b8eba4323a3f7aa54534263787745e5f3b
Maximilian Rink <maximilian.rink@telekom.de>
2026-06-22 03:59:42 +0200

    test: use context-aware listener in token redaction test

--- pkg/auth/manager_test.go
+++ pkg/auth/manager_test.go
@@ -91,7 +91,7 @@ func TestTokenManager_Acquire_RedactsSensitiveTokenURLParts(t *testing.T) {
 }
 
 func TestTokenManager_Acquire_RedactsSensitiveTokenURLPartsFromTransportError(t *testing.T) {
-	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	listener, err := (&net.ListenConfig{}).Listen(context.Background(), "tcp", "127.0.0.1:0")
 	if err != nil {
 		t.Fatal(err)
 	}
