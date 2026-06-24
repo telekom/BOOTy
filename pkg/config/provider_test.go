84562dc54c367dee0170909597d5eaa1a68ea859
Maximilian Rink <maximilian.rink@telekom.de>
2026-06-24 02:04:33 +0200

    config: trim validated enum values

--- pkg/config/provider_test.go
+++ pkg/config/provider_test.go
@@ -346,6 +346,7 @@ func TestValidate(t *testing.T) {
 		{name: "valid overlay type", cfg: Config{Network: NetworkConfig{BGP: BGPConfig{OverlayType: "evpn-vxlan"}}}},
 		{name: "invalid overlay type", cfg: Config{Network: NetworkConfig{BGP: BGPConfig{OverlayType: "gre"}}}, wantErr: "invalid network.bgp.overlayType"},
 		{name: "valid cloud-init ds", cfg: Config{Provision: ProvisionConfig{CloudInit: CloudInitConfig{Datasource: "nocloud"}}}},
+		{name: "trims cloud-init ds", cfg: Config{Provision: ProvisionConfig{CloudInit: CloudInitConfig{Datasource: " NoCloud "}}}},
 		{name: "configdrive cloud-init ds is unsupported", cfg: Config{Provision: ProvisionConfig{CloudInit: CloudInitConfig{Datasource: "configdrive"}}}, wantErr: "invalid provision.cloudInit.datasource"},
 		{name: "invalid cloud-init ds", cfg: Config{Provision: ProvisionConfig{CloudInit: CloudInitConfig{Datasource: "ec2"}}}, wantErr: "invalid provision.cloudInit.datasource"},
 		{name: "valid sysext preload mode", cfg: Config{Provision: ProvisionConfig{Sysext: SysextConfig{DefaultMode: "preload"}}}},
