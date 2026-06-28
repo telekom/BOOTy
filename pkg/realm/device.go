daca1185808faa5e4207eaf45e49af0eb5a92bd4
Maximilian Rink <maximilian.rink@telekom.de>
2026-06-29 00:30:58 +0200

    fix: quote device path in mknod error

--- pkg/realm/device.go
+++ pkg/realm/device.go
@@ -57,7 +57,7 @@ func (d *Devices) CreateDevice() error {
 		if d.Device[x].CreateDevice {
 			err := syscall.Mknod(d.Device[x].Path, d.Device[x].Mode, makedev(d.Device[x].Major, d.Device[x].Minor))
 			if err != nil {
-				return fmt.Errorf("create device %q at %s: %w", d.Device[x].Name, d.Device[x].Path, err)
+				return fmt.Errorf("create device %q at %q: %w", d.Device[x].Name, d.Device[x].Path, err)
 			}
 		}
 	}
